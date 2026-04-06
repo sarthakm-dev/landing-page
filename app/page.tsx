9259e06c3e45ceccc65b2220beb3f48c412a4512
Sarthak Mishra <sarthakm@mindfiresolutions.com>
2026-04-06 17:43:22 +0530

    fix(ui): update page css styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import { ContactSection } from "@/app/components/landing/contact-section";
-import { FeaturesSection } from "@/app/components/landing/features-section";
-import { HeroSection } from "@/app/components/landing/hero-section";
-
+import { ContactSection } from "@/app/components/landing/ContactSection";
+import { FeaturesSection } from "@/app/components/landing/FeaturesSection";
+import { HeroSection } from "./components/landing/HeroSection";
 
 export default function Home() {
   return (
-    <main className=" bg-[#121212] text-white">
+    <main className=" bg-(--color-background) text-white">
       <div className="mx-auto flex w-full max-w-295 flex-col gap-6 px-4 py-4 sm:px-6 lg:px-8">
         <HeroSection />
         <FeaturesSection />
