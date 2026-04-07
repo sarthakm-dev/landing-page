49020fefe740789bb764702c936184954e96ee7e
Sarthak Mishra <sarthakm@mindfiresolutions.com>
2026-04-07 12:13:18 +0530

    fix(ui): update min width

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import BusinessSection from "./components/landing/BusinessSection";
 
 export default function Home() {
   return (
-    <main className=" bg-(--color-background) text-white">
-      <div className="mx-auto flex w-full max-w-295 flex-col px-4 py-4 sm:px-6 lg:px-8">
+    <main className=" bg-(--color-background) text-white overflow-hidden">
+      <div className="mx-auto flex min-w-full  flex-col">
         <HeroSection />
         <FeaturesSection />
         <BusinessSection/>
